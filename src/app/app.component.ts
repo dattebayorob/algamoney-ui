6ba793a17f70d96303138c922167e2639903afb3
dattebayoRob <robson.william65@gmail.com>
2019-03-29 16:36:15 -0300

    p-dataTable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'algamoney-ui';
+
+  lancamentos = [
+    {tipo: 'DESPESA', descricao: 'Teste de Despesa', dataVencimento: '29/03/2018',
+     dataPagamento: null, valor: 4.55, pessoa: 'Cobrador'},
+    {tipo: 'DESPESA', descricao: 'Teste de Despesa 2', dataVencimento: '02/04/2018',
+     dataPagamento: null, valor: 4.55, pessoa: 'Cobrador'},
+    {tipo: 'RECEITA', descricao: 'Teste de RECEITA', dataVencimento: '29/03/2018',
+     dataPagamento: null, valor: 4.55, pessoa: 'Pagador porreta'}
+  ];
 }
