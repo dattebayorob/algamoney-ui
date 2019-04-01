5b7ebb3b15b08bfabb6d25eb0b0909db4501fac9
dattebayoRob <robson.william65@gmail.com>
2019-03-31 22:28:49 -0300

    tests with template-driven

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
+class Profissao {
+  id: number;
+  nome: string;
+}
+class Usuario {
+  nome: string;
+  email: string;
+  profissaoId: number;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +17,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'algamoney-ui';
+  usuario = {};
+  profissoes = [
+    { id: 1, nome: 'Engenheiro' }, { id: 2, nome: 'Teco'}, {id: 3, nome: 'Programador'}
+  ];
+  save(form: NgForm){
+    console.log(this.usuario);
+    console.log(form.value);
+    console.log(this.usuario);
+  }
 }
