f47b2f65460d883659ba80d1a077dc402e29a23d
dattebayoRob <robson.william65@gmail.com>
2019-03-31 21:43:28 -0300

    Format dates and currency tipes with pipes

--- src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LancamentosPesquisaComponent {
   lancamentos = [
-    {tipo: 'DESPESA', descricao: 'Teste de Despesa', dataVencimento: '29/03/2018',
+    {tipo: 'DESPESA', descricao: 'Teste de Despesa', dataVencimento: new Date(2018, 3, 29),
      dataPagamento: null, valor: 4.55, pessoa: 'Cobrador'},
-    {tipo: 'DESPESA', descricao: 'Teste de Despesa 2', dataVencimento: '02/04/2018',
+    {tipo: 'DESPESA', descricao: 'Teste de Despesa 2', dataVencimento: new Date(2019, 3, 29),
      dataPagamento: null, valor: 4.55, pessoa: 'Cobrador'},
-    {tipo: 'RECEITA', descricao: 'Teste de RECEITA', dataVencimento: '29/03/2018',
+    {tipo: 'RECEITA', descricao: 'Teste de RECEITA', dataVencimento: new Date(2019, 3, 3),
      dataPagamento: null, valor: 4.55, pessoa: 'Pagador porreta'}
   ];
 
